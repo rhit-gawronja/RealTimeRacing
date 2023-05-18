d0163baa8c23ddcd3f5bbc9d7ba6098e2b123011
Joey gawron <gawronja@rose-hulman.edu>
2023-05-18 19:47:16 -0400

    adding in the map stuff

--- functions/public/scripts/raceLobby.js
+++ functions/public/scripts/raceLobby.js
@@ -8,15 +8,18 @@ async function getCSRF() {
       return data.csrfToken;
     });
 }
+csrf=getCSRF();
+function lon2tile(lon,zoom) { return (Math.floor((lon+180)/360*Math.pow(2,zoom))); }
+ function lat2tile(lat,zoom)  { return (Math.floor((1-Math.log(Math.tan(lat*Math.PI/180) + 1/Math.cos(lat*Math.PI/180))/Math.PI)/2 *Math.pow(2,zoom))); }
 navigator.geolocation.getCurrentPosition(function(position) {
   var currentLat = position.coords.latitude;
   var currentLng = position.coords.longitude;
 
   // Create the map
-  var map = L.map('map').setView([currentLat, currentLng], 13);
-
+  var map = L.map('map').setView([currentLat, currentLng], 10);
+	mapStr=`https://tile.openstreetmap.org/15/${lon2tile(currentLng,15)}/${lat2tile(currentLat,15)}.png`
   // Add the OpenStreetMap tile layer
-  L.tileLayer('https://%7Bs%7D.tile.openstreetmap.org/%7Bz%7D/%7Bx%7D/%7By%7D.png', {
+  L.tileLayer(mapStr, {
     attribution: 'Map data © OpenStreetMap contributors'
   }).addTo(map);
 
@@ -24,12 +27,16 @@ navigator.geolocation.getCurrentPosition(function(position) {
   var currentMarker = L.marker([currentLat, currentLng]).addTo(map);
   var destinationMarker = L.marker([destinationLat, destinationLng]).addTo(map);
 
+  console.log('LatStart: '+currentLat+'lonStart:'+currentLng)
+  console.log('LatEnd: '+destinationLat+'lonEnd:'+destinationLng)
   // Create a routing control instance
   var routingControl = L.Routing.control({
     waypoints: [
       L.latLng(currentLat, currentLng),
       L.latLng(destinationLat, destinationLng)
+
     ],
+    router: L.Routing.mapbox('sk.eyJ1IjoiZ2F3cm9uamEiLCJhIjoiY2xodHF5cnFxMzh3MzNycWtqYnVlajVucyJ9.Dj3AELyPknstTxq_hw7Lgg'),
     routeWhileDragging: true
   }).addTo(map);
 
@@ -42,7 +49,7 @@ async function getRaceLobby(){
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
-      "CSRF-Token": getCSRF(),
+      "CSRF-Token": csrf,
     },
 
   })
