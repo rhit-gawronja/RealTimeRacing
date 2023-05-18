8cc507c5cfd81198eefb6a45b8d6416d05ca7c1e
Joey gawron <gawronja@rose-hulman.edu>
2023-05-18 06:22:19 -0400

    race page

--- functions/public/scripts/racePage.js
+++ functions/public/scripts/racePage.js
@@ -5,9 +5,48 @@ async function getCSRF() {
       return data.csrfToken;
     });
 }
+// const raceWatcher =navigator.geolocation.watchPosition(async(position)=>{
+// let csrfToken = await getCSRF();
+//   fetch('/raceData',{
+//     method:'GET',
+//     headers:{
+//       "Consent-Type":"application/json",
+//       "CSRF-Token": csrfToken,
+//     },
+//     body:JSON.stringify({
+//       latitude: position.coords.latitude,
+//       longitude: position.coords.longitude,
+//     }),
+//       .then((response)=>response.json())
+//       .then((data)=>{
+//         //screen update section
+//       })
+//       .catch((error)=>{
+//         console.error(error);
+//       });
+//   });
+
+// });
+function updateRace(driver1,driver2){
+  const statBoard=document.getElementById("statsBoard");
+statBoard.innerHTML=`<table class="table"><thead><tr><th scope="col">place</th><th scope="col">name</th><th scope="col">speed</th></tr></thead> <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>${driver1}</td>
+      <td>Otto</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>${driver2}</td>
+      <td>Thornton</td>
+    </tr>
+      </tbody>
+</table>`
+}
 
 const watchID = navigator.geolocation.watchPosition(async (position) => {
   let csrfToken = await getCSRF();
+  updateRace(position.coords.latitude,position.coords.longitude);
   console.log(position.coords.latitude, position.coords.longitude);
   fetch("/updateLocation", {
     method: "PUT",
